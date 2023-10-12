61e2898f98db0098c6aec3c13ded9a44f030c355
saiganeshk15 <saiganeshk15@gmail.com>
2023-10-12 20:55:26 +0530

    data hidden

--- round4.js
+++ round4.js
@@ -1,11 +1,3 @@
-const data = {
-    0 : ["Enter your name" , "ganesh"],
-    1 : ["Enter your gender?", "male"],
-    2 : ["Age?" , "48"],
-    3 : ["Class","vbit"],
-    4 : ["something","everything"]
-}
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
