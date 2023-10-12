eaf62ad298f3080d86205643f442fa1e8c04da99
saiganeshk15 <saiganeshk15@gmail.com>
2023-10-12 22:22:29 +0530

    data

---
+++ data.js
@@ -0,0 +1,30 @@
+const data = {
+    0 : {
+        0 : ["Enter your name" , "ganesh"],
+        1 : ["Enter your gender?", "male"],
+        2 : ["Age?" , "48"],
+        3 : ["Class","vbit"],
+        4 : ["something","everything"]
+    },
+    1 : {
+        0 : ["Enter your name" , "ganesh"],
+        1 : ["Enter your gender?", "male"],
+        2 : ["Age?" , "48"],
+        3 : ["Class","vbit"],
+        4 : ["something","everything"]
+    },
+    2 : {
+        0 : ["Enter your name" , "ganesh"],
+        1 : ["Enter your gender?", "male"],
+        2 : ["Age?" , "48"],
+        3 : ["Class","vbit"],
+        4 : ["something","everything"]
+    },
+    3 : {
+        0 : ["Enter your name" , "ganesh"],
+        1 : ["Enter your gender?", "male"],
+        2 : ["Age?" , "48"],
+        3 : ["Class","vbit"],
+        4 : ["something","everything"]
+    }
+}
