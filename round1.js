765533f378ec2a566a9be7b09e784ce7aea6937e
saiganeshk15 <saiganeshk15@gmail.com>
2023-10-14 19:40:52 +0530

    constraints added successfully

--- round1.js
+++ round1.js
@@ -1,37 +1,106 @@
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 var qst = document.getElementById("round1");
-var ans = document.getElementById("round-1")
-var incorrect = document.getElementById('incorrect');
-
+var ans = document.getElementById("round-1");
+var incorrect = document.getElementById("incorrect");
 
 async function getDataFromFirestore() {
-    const db = firebase.firestore();
+  const db = firebase.firestore();
+  const user = await db.collection("user");
 
-      const querySnapshot = await db.collection('data').get();
-      const dataArray = querySnapshot.docs.map((doc) => doc.data());
-      var questionAnswerId = getRandomInt(dataArray.length)
-      qst.innerHTML = dataArray[questionAnswerId]['question']
+  if (localStorage.getItem("userId") == null) {
 
-      document.getElementById("round-1").addEventListener('keyup', (e) => {
-        if (e.target.value.toLowerCase() == dataArray[questionAnswerId]['answer'].toLowerCase() ) {
-            document.querySelector('#submit').style.display = 'block';
-        }
-        else {
-            document.querySelector('#submit').style.display = 'none';
-        }
-    })
-    document.getElementById('submit').addEventListener('click', (e) => {
-        e.preventDefault();
-        var answer = document.getElementById("round-1").value;
-        if ( answer.toLowerCase() == dataArray[questionAnswerId]['answer'].toLowerCase()) {
-            setTimeout(3000, alert("Congratulations, you have completed the first round"))
-            window.location.replace("round2.html")
-        } else {
-            incorrect.innerHTML = 'Incorrect answer';
-        }
-    })
+    const querySnapshot = await db.collection("data").get();
+    const dataArray = querySnapshot.docs.map((doc) => doc.data());
+    const questionAnswerId = getRandomInt(dataArray.length);
+
+    user
+      .add({
+        isStarted: true,
+        round: 1,
+        r1q : dataArray[questionAnswerId]["question"],
+        r1a : dataArray[questionAnswerId]["answer"]
+      })
+      .then((userId) => {
+        console.log(userId.id);
+        localStorage.setItem("userId", userId.id);
+      });
+    
+    console.log(dataArray[questionAnswerId]["question"], dataArray[questionAnswerId]["answer"])
+    qst.innerHTML = dataArray[questionAnswerId]["question"];
+
+    document.getElementById("round-1").addEventListener("keyup", (e) => {
+      if (
+        e.target.value.toLowerCase() ==
+        dataArray[questionAnswerId]["answer"].toLowerCase()
+      ) {
+        document.querySelector("#submit").style.display = "block";
+      } else {
+        document.querySelector("#submit").style.display = "none";
+      }
+    });
+    document.getElementById("submit").addEventListener("click", (e) => {
+      e.preventDefault();
+      var answer = document.getElementById("round-1").value;
+      if (
+        answer.toLowerCase() ==
+        dataArray[questionAnswerId]["answer"].toLowerCase()
+      ) {
+        setTimeout(
+          3000,
+          alert("Congratulations, you have completed the first round")
+        );
+        window.location.replace("round2.html");
+      } else {
+        incorrect.innerHTML = "Incorrect answer";
+      }
+    });
+  } else {
+
+    const userRef = user.doc(localStorage.getItem('userId'));
+    const userSnapshot = await userRef.get();
+    const userData = userSnapshot.data();
+    
+    if (userData.round == 2) {
+        window.location.href = "round2.html"
+    } else if (userData.round == 3) {
+        window.location.href = "round3.html"
+    } else if (userData.round == 4) {
+        window.location.href = "round4.html"
+    }
+    
+    qst.innerHTML = userData.r1q;
+
+    document.getElementById("round-1").addEventListener("keyup", (e) => {
+      if (
+        e.target.value.toLowerCase() ==
+        userData.r1a.toLowerCase()
+      ) {
+        document.querySelector("#submit").style.display = "block";
+      } else {
+        document.querySelector("#submit").style.display = "none";
+      }
+    });
+    document.getElementById("submit").addEventListener("click", (e) => {
+      e.preventDefault();
+      var answer = document.getElementById("round-1").value;
+      if (
+        answer.toLowerCase() ==
+        userData.r1a.toLowerCase()
+      ) {
+        setTimeout(
+          3000,
+          alert("Congratulations, you have completed the first round")
+        );
+        window.location.replace("round2.html");
+      } else {
+        incorrect.innerHTML = "Incorrect answer";
+      }
+    });
+  }
+
+  console.log(localStorage.getItem("userId"));
 }
-getDataFromFirestore()
+getDataFromFirestore();
